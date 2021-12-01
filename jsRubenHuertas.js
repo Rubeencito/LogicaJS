8ceba0cc7f9fb254e48c44fb65db30f5090fb1b1
Rubeencito <ruben.huertas@institutvidreres.cat>
2021-12-01 22:14:56 +0100

    EX7

--- jsRubenHuertas.js
+++ jsRubenHuertas.js
@@ -1,29 +1,14 @@
-var num1 = prompt("Pon el primer numero")
-var num2 = prompt("Pon el segundo numero")
-var num3 = prompt("Pon el tercer numero")
-
-if (num1 > num2 && num2 > num3 && num1 > num3) {
-    console.log(num1 + " " + num2 + " " + num3)
+var x = prompt ("Pon un numero");
+var valor = 0;
+while (valor < x) {
+    valor++
+    if (valor%2 === 0) {
+        console.log("Es parell " + valor)
+    }
+    else if (valor%2 !== 0){
+        console.log("Es senar " + valor)
+    }
 }
-if (num1 > num2 && num2 < num3 && num1 > num3) {
-    console.log(num1 + " " + num3 + " " + num2)
-} 
-
-
-if (num2 > num3 && num1 > num3 && num2 > num1) {
-    console.log(num2 + " " + num1 + " " + num3)
-} 
-if (num2 > num3 && num1 < num3 && num2 > num1) {
-    console.log(num2 + " " + num3 + " " + num1)
-} 
-
-
-if (num3 > num1 && num2 > num1 && num3 > num2) {
-    console.log(num3 + " " + num2 + " " + num1)
-} 
-if (num3 > num1 && num2 < num1 && num3 > num2) {
-    console.log(num3 + " " + num1 + " " + num2)
-} 
 
 
 
